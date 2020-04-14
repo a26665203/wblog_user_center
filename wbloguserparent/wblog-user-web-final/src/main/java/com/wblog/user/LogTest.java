f7e2523e693b5945bfb139ccd1b4374daa54e0db
a26665203 <2252065851@qq.com>
2020-04-14 17:37:30 +0800

    html试图解析器配置

--- wbloguserparent/wblog-user-web-final/src/main/java/com/wblog/user/LogTest.java
+++ wbloguserparent/wblog-user-web-final/src/main/java/com/wblog/user/LogTest.java
@@ -1,11 +1,18 @@
 package com.wblog.user;
 
+import com.wblog.user.controller.TestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.awt.*;
 
 public class LogTest {
+    @Autowired
+    TestController testController;
     private final static Logger logger = LoggerFactory.getLogger(LogTest.class);
     public static void main(String[] args){
+        LogTest  logTest = new LogTest();
         logger.info("别啊");
     }
 }
